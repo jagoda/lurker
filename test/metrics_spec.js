69d541a8c1c0f7beda57e5f324ff38e6df338366
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-07-23 12:18:20 -0400

    Metrics plugin.

---
+++ test/metrics_spec.js
@@ -0,0 +1,164 @@
+"use strict";
+var Hapi      = require("hapi");
+var Lab       = require("lab");
+var metrics   = require("../lib/metrics");
+var Nipple    = require("nipple");
+var nock      = require("nock");
+var Q         = require("q");
+var sinon     = require("sinon");
+var utilities = require("../lib/utilities");
+
+var after    = Lab.after;
+var before   = Lab.before;
+var describe = Lab.describe;
+var expect   = Lab.expect;
+var it       = Lab.it;
+
+describe("The metrics plugin", function () {
+
+	it("has a name", function (done) {
+		expect(metrics.register.attributes, "no name")
+		.to.have.property("name", "metrics");
+
+		done();
+	});
+
+	it("has a version", function (done) {
+		expect(metrics.register.attributes, "no version")
+		.to.have.property("version");
+
+		done();
+	});
+
+	describe("creating a new event", function () {
+		var server;
+
+		before(function (done) {
+			nock.disableNetConnect();
+			server = new Hapi.Server("localhost", 0);
+
+			Q.all([
+				Q.ninvoke(server.pack, "register", utilities),
+				Q.ninvoke(server.pack, "register", metrics)
+			])
+			.then(function () {
+				// Start the server to complete plugin lodaing.
+				return Q.ninvoke(server, "start");
+			})
+			.nodeify(done);
+		});
+
+		after(function (done) {
+			nock.enableNetConnect();
+			Q.ninvoke(server, "stop").nodeify(done);
+		});
+
+		describe("without error", function () {
+			var cubeRequest;
+			var failure;
+
+			before(function (done) {
+				cubeRequest  = nock("http://localhost:1080")
+				.post(
+					"/1.0/event/put",
+					[
+						{
+							type : "event",
+							id   : "id",
+							data : {
+								payload : "data"
+							}
+						}
+					]
+				)
+				.reply(200, {});
+
+				server.methods.createEvent(
+					{
+						type : "event",
+						id   : "id",
+						data : {
+							payload : "data"
+						}
+					},
+					function (error) {
+						failure = error;
+						done();
+					}
+				);
+			});
+
+			after(function (done) {
+				nock.cleanAll();
+				done();
+			});
+
+			it("returns successfully", function (done) {
+				expect(cubeRequest.isDone(), "no cube request").to.be.true;
+				expect(failure, "failed").not.to.exist;
+				done();
+			});
+		});
+
+		describe("encountering a request error", function () {
+			var cubeRequest;
+			var failure;
+
+			before(function (done) {
+				cubeRequest = nock("http://localhost:1080")
+				.post("/1.0/event/put")
+				.reply(500, "boom!");
+
+				server.methods.createEvent({}, function (error) {
+					failure = error;
+					done();
+				});
+			});
+
+			after(function (done) {
+				nock.cleanAll();
+				done();
+			});
+
+			it("returns an error", function (done) {
+				expect(failure, "no error").to.be.an.instanceOf(Error);
+
+				expect(failure.message, "error message")
+				.to.match(/request failed/i);
+
+				done();
+			});
+		});
+
+		describe("encountering a connection error", function () {
+			var failure;
+			var postStub;
+
+			before(function (done) {
+				postStub = sinon.stub(
+					Nipple, "post",
+					function (uri, options, callback) {
+						callback(new Error("boom!"));
+					}
+				);
+
+				server.methods.createEvent({}, function (error) {
+					failure = error;
+					done();
+				});
+			});
+
+			after(function (done) {
+				postStub.restore();
+				done();
+			});
+
+			it("returns an error", function (done) {
+				expect(failure, "no error").to.be.an.instanceOf(Error);
+				expect(failure.message).to.match(/boom!/i);
+
+				done();
+			});
+		});
+	});
+});
