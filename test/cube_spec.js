538c57b9855772129bd43dceeb66487fc2e6a6f8
Jeff Jagoda <jjagoda@bandwidth.com>
2014-08-04 13:10:25 -0400

    Cube collector pass-through API.

--- test/cube_spec.js
+++ test/cube_spec.js
@@ -1,4 +1,5 @@
 "use strict";
+var Browser = require("zombie");
 var cube    = require("../lib/cube");
 var Lab     = require("lab");
 var nock    = require("nock");
@@ -20,6 +21,10 @@ describe("The cube plugin", function () {
 });
 
 describe("The cube pass-through API", function () {
+	var TOKEN         = "atoken";
+	var AUTHORIZATION = "token " + TOKEN;
+	var GITHUB_API    = "https://api.github.com";
+	var USERNAME      = "octocat";
 
 	before(function (done) {
 		nock.disableNetConnect();
@@ -31,5 +36,86 @@ describe("The cube pass-through API", function () {
 		done();
 	});
 
-	// TODO: add API endpoints
+	describe("/collector", function () {
+		var browser;
+
+		function collectorRequest () {
+			return nock("http://localhost:1080")
+			.get("/")
+			.reply(200, "cube collector");
+		}
+
+		before(function (done) {
+			browser = new Browser();
+			done();
+		});
+
+		describe("when authenticated", function () {
+			var request;
+			var response;
+
+			before(function (done) {
+				request = collectorRequest();
+
+				nock(GITHUB_API)
+				.get(
+					"/applications/" + process.env.CLIENT_ID + "/tokens/" +
+					TOKEN
+				)
+				.reply(200, {
+					user : { login : USERNAME }
+				});
+
+				browser.http({
+					headers : {
+						"Authorization": AUTHORIZATION
+					},
+
+					url : "/collector"
+				})
+				.then(function (_response_) {
+					response = _response_;
+				})
+				.nodeify(done);
+			});
+
+			after(function (done) {
+				nock.cleanAll();
+				done();
+			});
+
+			it("forwards requests to the cube collector", function (done) {
+				expect(request.isDone(), "cube request").to.be.true;
+				expect(response.statusCode, "status").to.equal(200);
+				expect(response.payload, "payload").to.equal("cube collector");
+				done();
+			});
+		});
+
+		describe("when not authenticated", function () {
+			var request;
+			var response;
+
+			before(function (done) {
+				request = collectorRequest();
+
+				browser.http({ url : "/collector" })
+				.then(function (_response_) {
+					response = _response_;
+				})
+				.nodeify(done);
+			});
+
+			after(function (done) {
+				nock.cleanAll();
+				done();
+			});
+
+			it("rejects requests", function (done) {
+				expect(request.isDone(), "cube request").to.be.false;
+				expect(response.statusCode, "status").to.equal(401);
+				done();
+			});
+		});
+	});
 });
--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,17 +1,34 @@
 "use strict";
 var Browser     = require("zombie");
 var Environment = require("./Environment");
+var Good        = require("good");
 var Lab         = require("lab");
 var mummy       = require("mummy");
 var path        = require("path");
+var sinon       = require("sinon");
 
+var after  = Lab.after;
 var before = Lab.before;
 
-before(function (done) {
-	var environment = new Environment();
+var environment = new Environment();
 
+before(function (done) {
+	environment.set("CLIENT_ID", "aclientid");
+	environment.set("CLIENT_SECRET", "aclientsecret");
 	environment.set("SECRET", "asecret");
 	Browser.default.silent = true;
+
+	// Squelch logging for the test run...
+	sinon.stub(Good, "register", function (plugin, options, done) {
+		done();
+	});
+
 	mummy.extend(path.join(__dirname, "..", "..", "lib", "server.json"))
 	.nodeify(done);
+});
+
+after(function (done) {
+	Good.register.restore();
+	environment.restore();
+	done();
 });
