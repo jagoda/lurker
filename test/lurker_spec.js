67870db410eafaf79a9745248657b5aa82548605
Jeff Jagoda <jjagoda@bandwidth.com>
2014-10-19 20:55:02 -0400

    Generate Grafana config.
    Closes #1

--- test/lurker_spec.js
+++ test/lurker_spec.js
@@ -8,7 +8,7 @@ var Mummy   = require("mummy");
 var Q       = require("q");
 var Sinon   = require("sinon");
 
-describe("The lurker plugin", function () {
+describe("The Lurker plugin", function () {
 	it("has a name", function () {
 		expect(Lurker.register.attributes, "name")
 		.to.have.property("name", "lurker");
@@ -117,7 +117,7 @@ describe("The lurker plugin", function () {
 	});
 });
 
-describe("The lurker status page", function () {
+describe("The Lurker status page", function () {
 	var browser;
 
 	before(function (done) {
@@ -128,5 +128,56 @@ describe("The lurker status page", function () {
 
 	it("shows the Grafana dashboard", function () {
 		expect(browser.text("title"), "title").to.match(/grafana/i);
+	});
+});
+
+describe("The Grafana configuration", function () {
+	var settings;
+
+	before(function (done) {
+		var browser = new Browser();
+
+		browser.http({
+			method : "GET",
+			url    : "/config.js"
+		})
+		.then(function (response) {
+			/* jshint -W054 */
+			var load   = new Function("define", response.payload);
+			/* jshint +W054 */
+
+			function define (deps, factory) {
+				function Settings (options) {
+					return options;
+				}
+
+				settings = factory(Settings);
+			}
+
+			load(define);
+		})
+		.nodeify(done);
+	});
+
+	it("use the Lurker InfluxDB as the default data store", function () {
+		expect(settings.datasources, "lurker db")
+		.to.have.property("lurker").that.deep.equals({
+			default  : true,
+			type     : "influxdb",
+			url      : "http://localhost:8086/db/lurker",
+			username : "lurker",
+			password : "test"
+		});
+	});
+
+	it("uses the Lurker InfluxDB as the Grafana DB", function () {
+		expect(settings.datasources, "grafana db")
+		.to.have.property("grafana").that.deep.equals({
+			grafanaDB : true,
+			type      : "influxdb",
+			url       : "http://localhost:8086/db/grafana",
+			username  : "lurker",
+			password  : "test"
+		});
 	});
 });
